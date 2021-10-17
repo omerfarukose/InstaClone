cbf670a1abfbcd1d6030699c1871342b6a96a82f
Ömer Faruk Köse <kazakh_sarbas@hotmail.com>
2021-10-17 08:42:32 +0300

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Ömer Faruk KÖSE on 17.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
